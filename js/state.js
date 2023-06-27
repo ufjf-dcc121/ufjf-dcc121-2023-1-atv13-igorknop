693dcb0aac6e5c7f15deb4c5a574cb86ce65e8d0
Igor Knop <igor.knop@ufjf.br>
2023-06-27 20:19:10 -0300

    adciona efeito no estado

--- js/state.js
+++ js/state.js
@@ -8,5 +8,10 @@ export function mover(de, para) {
     if (Math.abs(para - de) >= 3) {
         return false;
     }
+    if(tabuleiro[para]!==null){
+        return false;
+    }
+    tabuleiro[para] = tabuleiro[de];
+    tabuleiro[de] = null;
     return true;
 }
